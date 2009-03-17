6d59bffb91f7914b1e476679b60ab777cdf07d22
idlesign <idlesign@yandex.ru>
2009-03-17 12:19:56 +0000

    * css collapse controls style fix * _toggle method rewritten (now supports initially collapsed mode for slide panel types)

--- ui.panel.js
+++ ui.panel.js
@@ -1,13 +1,11 @@
 /*
- * Panel Draft 0.2
+ * Panel Draft 0.2.1
  * for jQuery UI
  *
  * Copyright (c) 2009 idle sign
  *
  * Depends:
  *	ui.core.js
- *	effects.core.js
- *	effects.slide.js
  */
 (function($) {
 
@@ -52,45 +50,38 @@
 					switch (o.collapseType) {
 						case 'slide-right':
 							this.rightBox.append('<span><span/></span>');
-							this.collapsePanel = this.rightBox.children().eq(1)
-								.addClass(o.collapsePnlClass)
-								.css({
-										'position':'relative',
-										'top':'-0.1em',
-										'left':'0.1em'
-									});
+							this.collapsePanel = this.rightBox.children().eq(1).addClass(o.collapsePnlClass);
 							this.collapseButton =  this.collapsePanel.children().eq(0).addClass(o.slideRIcon);
 							this.iconBtnClpsd = o.slideRIconClpsd;
 							this.iconBtn = o.slideRIcon;
+							this.ctrlBox = this.controlsBox;
 							break;
 						case 'slide-left':
 							this.headerBox.prepend('<span><span/></span>');
-							this.collapsePanel = this.headerBox.children().eq(0)
-								.addClass(o.collapsePnlClass)
-								.css({
-										'position':'relative',
-										'top':'-0.1em',
-										'left':'-0.1em'
-									});
+							this.collapsePanel = this.headerBox.children().eq(0).addClass(o.collapsePnlClass);
 							this.collapseButton =  this.collapsePanel.children().eq(0).addClass(o.slideLIcon);
 							this.iconBtnClpsd = o.slideLIconClpsd;
 							this.iconBtn = o.slideLIcon;
+							this.ctrlBox = this.rightBox;
 							break;
 						default:
 							this.headerBox.prepend('<span><span/></span>');
 							this.collapseButton = this.headerBox.children().eq(0).addClass(o.headerIcon);
 							this.iconBtnClpsd = o.headerIconClpsd;
 							this.iconBtn = o.headerIcon;
+							this.ctrlBox = this.controlsBox;
 							break;
 					}
 
-					var toggleFn = this._toggle();
+					var toggleFn = this._toggle(o.collapseSpeed);
 					this._buttonHover(this.collapseButton);
 					this.collapseButton.addClass(o.iconClass).bind('click', toggleFn );
 					this.titleTextBox.bind('click', toggleFn );
 
 					// panel collapsed
-					if (o.collapsed) $( this._toggle(true) );
+					if (o.collapsed) {
+						$( this._toggle(0) );
+					}
 				}
 
 				//alert(this.titleBox.html());
@@ -99,7 +90,7 @@
 
 		},
 
-		_toggle: function (hide){
+		_toggle: function (collapseSpeed){
 			var o = this.options,
 				btn = this.collapseButton,
 				ibc = this.iconBtnClpsd,
@@ -108,62 +99,38 @@
 				contentBox = this.contentBox,
 				headerBox = this.headerBox,
 				titleTextBox = this.titleTextBox,
-				animate = '', ctrlBox = '';
+				ctrlBox = this.ctrlBox;
+			
+			return function(){				
+				if (ctrlBox) ctrlBox.toggle(0);
+				if (o.collapseType=='default'){
+					// different content sliding animation
+					if (collapseSpeed==0) {
+						if (ctrlBox) ctrlBox.hide();
+						contentBox.hide();
+					} else contentBox.slideToggle(collapseSpeed);
+				} else {
+					if (collapseSpeed==0) {
+						// reverse collapsed option for immediate folding
+						o.collapsed=false;
+						if (ctrlBox) ctrlBox.hide();
+						contentBox.hide();
+					} else contentBox.toggle();
 
-			if (hide){
-				animate = function(){
-					// simple hide w/o animation
-					contentBox.hide();
+					// vertical text workaround - to be replaced with more clever
+					if (o.collapsed==false){
+						headerBox.attr('align','center');
+						titleTextBox.html(titleTextBox.text().replace(/(.)/g, '$1<BR>'));
+						panelBox.animate( {width: '2.4em'}, collapseSpeed );
+					} else {						
+						headerBox.attr('align','left');
+						titleTextBox.html(titleTextBox.text().replace(/<BR>/g, ' '));
+						panelBox.animate( {width: o.width}, collapseSpeed );
+					}
+
+					o.collapsed = !o.collapsed;
+
 				}
-			} else {
-				switch (o.collapseType) {
-					case 'slide-right':
-						ctrlBox = this.controlsBox;
-						animate = function(){
-							contentBox.toggle();
-							ctrlBox.toggle();
-							// vertical text workaround - to be replaced with more clever
-							if (o.collapsed==false){
-								headerBox.attr('align','center');
-								titleTextBox.html(titleTextBox.text().replace(/(.)/g, '$1<br/>'));
-								panelBox.animate( {width: '2.4em'}, o.collapseSpeed );
-								o.collapsed = true;
-							} else {
-								headerBox.attr('align','left');
-								titleTextBox.html(titleTextBox.text().replace(/<BR>/g, ' '));
-								panelBox.animate( {width: o.width}, o.collapseSpeed );
-								o.collapsed = false;
-							}
-						}
-						break;
-					case 'slide-left':
-						ctrlBox = this.rightBox;
-						animate = function(){						
-							contentBox.toggle();
-							ctrlBox.toggle();
-							// vertical text workaround - to be replaced with more clever
-							if (o.collapsed==false){								
-								headerBox.attr('align','center');
-								titleTextBox.html(titleTextBox.text().replace(/(.)/g, '$1<BR>'));
-								panelBox.animate( {width: '2.4em'}, o.collapseSpeed );
-								o.collapsed = true;
-							} else {
-								headerBox.attr('align','left');
-								titleTextBox.html(titleTextBox.text().replace(/<BR>/g, ' '));
-								panelBox.animate( {width: o.width}, o.collapseSpeed );
-								o.collapsed = false;
-							}
-						}
-						break;
-					default:
-						animate = function () {
-							contentBox.slideToggle(o.collapseSpeed);
-						}
-						break;
-				}
-			}
-			return function(){
-				$(animate);
 				btn.toggleClass(ibc).toggleClass(ib);
 			}
 		},
@@ -192,7 +159,7 @@
 	});
 
 	$.extend($.ui.panel, {
-		version: '0.2',
+		version: '0.2.1',
 		defaults: {
 			collapsible: true,
 			collapseType: 'default',
